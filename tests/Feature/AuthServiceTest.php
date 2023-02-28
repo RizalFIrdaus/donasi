3f58d87407cbda5d9afb2b10fe1607e3440c77a8
rizal <rizal.firdaus117@gmail.com>
2023-02-28 16:56:11 +0700

    unit test auth service register

--- tests/Feature/AuthServiceTest.php
+++ tests/Feature/AuthServiceTest.php
@@ -9,10 +9,38 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthServiceTest extends TestCase
 {
+    use WithFaker;
     private AuthService $authService;
     public function testAuthServiceProvider()
     {
         $this->authService = $this->app->make(AuthService::class);
         self::assertSame($this->authService, $this->app->make(AuthService::class));
+    }
+
+    public function testRegister()
+    {
+        $this->get("/user/register")
+            ->assertSeeText("Mulai donasi dengan mendaftar akun");
+    }
+
+    public function testFailedDoRegister()
+    {
+        $response = $this->post("/user/register", [
+            "name" => $this->faker->name,
+            "email" => $this->faker->email,
+            "password" => ""
+        ]);
+        $response->assertSessionHasErrors(["password"]);
+    }
+
+    public function testDoRegisterSuccess()
+    {
+        $response = $this->post("/user/register", [
+            "name" => $this->faker->name,
+            "email" => $this->faker->email,
+            "password" => "ArDDDDAdawd_117"
+        ]);
+
+        $response->assertRedirect("/user/login");
     }
 }
