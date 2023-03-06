abb8de389c4e5b9d8286c5333358174e3996a18b
rizal <rizal.firdaus117@gmail.com>
2023-03-06 11:38:15 +0700

    profile & social media migration

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     return view("Beranda.index");
-})->middleware("auth");
+});
 Route::middleware("auth")->group(function () {
     Route::post("/user/logout", [AuthController::class, "logout"])->name("logout");
 });
